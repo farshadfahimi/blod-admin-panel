77aa5a22ecde6b4d5af177fe4798710a16827547
Farshad fahimi <farshadfahimi1371@gmail.com>
2023-01-16 21:20:39 +0330

    remove comment on server hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,6 +1,5 @@
 import type { Handle } from "@sveltejs/kit";
 
 export const handle = (async ({ event, resolve }) => {
-  console.log({ section: 'server', event  })
   return await resolve(event)
 }) satisfies Handle

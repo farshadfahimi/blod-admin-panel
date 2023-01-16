0625d6463498be32a39ee7e166310753a9c15763
Farshad fahimi <farshadfahimi1371@gmail.com>
2023-01-16 21:19:43 +0330

    remove client hooks

--- src/hooks.client.ts
+++
@@ -1,6 +0,0 @@
-import type { Handle } from '@sveltejs/kit';
-
-export const handle = (async ({ event, resolve }) => {
-    console.log({ section: 'client', event})
-    return await resolve(event);
-}) satisfies Handle;
